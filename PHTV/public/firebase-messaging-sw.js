70acebdee667436264e88101b4d797e433051bb0
Phạm Hùng Tiến <hungtien10a7@gmail.com>
2026-01-10 16:49:46 +0700

    fix(sw): update notification icon path to include base directory

--- PHTV/public/firebase-messaging-sw.js
+++ PHTV/public/firebase-messaging-sw.js
@@ -19,7 +19,7 @@ messaging.onBackgroundMessage((payload) => {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/icon.webp'
+    icon: '/PHTV/icon.webp'
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
